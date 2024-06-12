04d4ba589e8aabf9cc3815284f6cbe101527c2c0
Szymon <kl4mk4@gmail.com>
2024-06-12 22:41:48 +0200

    Add logs functionality

--- skrypt.sh
+++ skrypt.sh
@@ -4,5 +4,14 @@ if [ "$1" == "--date" ]; then
   date
 fi
 
+if [ "$1" == "--logs" ]; then
+  num_files=${2:-100}
+  for i in $(seq 1 $num_files); do
+    echo "log$i.txt" > "log$i.txt"
+    echo "Created by skrypt.sh on $(date)" >> "log$i.txt"
+  done
+fi
+
+
 
 
